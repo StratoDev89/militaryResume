a9c71ac3c6ffa346d46782ea2edf6ed543e24cfc
StratoDev89 <danieltestingemail01@gmail.com>
2024-04-16 16:53:40 -0400

    refactor: generatePdf

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -45,52 +45,151 @@ export class FormComponent {
 
   async generatePdf() {
     try {
+      // const pdf = new jsPDF({});
+      // const toCanvas1 = document.querySelector('#toCanvas1') as HTMLElement;
+      // const toCanvas2 = document.querySelector('#toCanvas2') as HTMLElement;
+      // const toCanvas3 = document.querySelector('#toCanvas3') as HTMLElement;
+
+      // const maxHeight = (window.innerHeight / 607) * 2100;
+
+      // let count = 0;
+      // let firstPageComplete = false;
+      // let secondPageComplete = false;
+      // let threePageComplete = false;
+      // let paddingToSecondPage = false;
+      // let paddingToThirdtPage = false;
+
+      // const elements = document.querySelectorAll(
+      //   '#resume',
+      // ) as NodeListOf<HTMLElement>;
+
+      // elements.forEach((resumeElement) => {
+      //   const elementsToRemove = resumeElement.querySelectorAll('#remove');
+
+      //   elementsToRemove.forEach((removeElement) => {
+      //     removeElement.remove();
+      //   });
+      // });
+
+      // const elementsArray = Array.from(elements);
+
+      // elementsArray.forEach((elem) => {
+      //   if (elem.clientHeight > 0) {
+      //     elem.style.padding = '.5rem 3rem';
+      //   }
+      // });
+
+      // elementsArray[0].style.paddingTop = '4rem';
+
+      // elementsArray.forEach((elem, i) => {
+      //   const elemnHeight = elem.clientHeight;
+
+      //   count += elemnHeight;
+
+      //   if (count > maxHeight) {
+      //     firstPageComplete = true;
+      //   }
+
+      //   if (count > maxHeight * 1.5) {
+      //     secondPageComplete = true;
+      //   }
+
+      //   if (count < maxHeight && !firstPageComplete) {
+      //     toCanvas1.appendChild(elem.cloneNode(true));
+      //   }
+
+      //   if (count < maxHeight * 1.5 && firstPageComplete) {
+      //     if (!paddingToSecondPage) {
+      //       elementsArray[i].style.paddingTop = '4rem';
+      //     }
+
+      //     toCanvas2.appendChild(elem.cloneNode(true));
+      //     paddingToSecondPage = true;
+      //   }
+
+      //   if (secondPageComplete) {
+      //     if (!paddingToThirdtPage) {
+      //       elementsArray[i].style.paddingTop = '4rem';
+      //     }
+
+      //     toCanvas3.appendChild(elem.cloneNode(true));
+      //     paddingToThirdtPage = true;
+      //   }
+      // });
+
+      // const canvas1 = await html2canvas(toCanvas1, {
+      //   scale: 2,
+      // });
+      // const imgHeight1 = (canvas1.height * 210) / canvas1.width;
+
+      // const canvas2 = await html2canvas(toCanvas2, {
+      //   scale: 2,
+      // });
+
+      // const imgHeight2 = (canvas2.height * 210) / canvas2.width;
+
+      // const canvas3 = await html2canvas(toCanvas3, {
+      //   scale: 2,
+      // });
+
+      // const imgHeight3 = (canvas3.height * 210) / canvas3.width;
+
+      // pdf.addImage(
+      //   canvas1.toDataURL('image/png'),
+      //   'PNG',
+      //   0,
+      //   0,
+      //   210,
+      //   imgHeight1,
+      // );
+
+      // if (firstPageComplete) {
+      //   pdf.addPage();
+      //   pdf.addImage(
+      //     canvas2.toDataURL('image/png'),
+      //     'PNG',
+      //     0,
+      //     0,
+      //     210,
+      //     imgHeight2,
+      //   );
+      // }
+
+      // if (secondPageComplete) {
+      //   pdf.addPage();
+
+      //   pdf.addImage(
+      //     canvas3.toDataURL('image/png'),
+      //     'PNG',
+      //     0,
+      //     0,
+      //     210,
+      //     imgHeight3,
+      //   );
+      // }
+
+      // pdf.save('resume.pdf');
+
       const pdf = new jsPDF({});
+      const elements = document.querySelectorAll(
+        '#resume',
+      ) as NodeListOf<HTMLElement>;
+
+      const maxHeight = (window.innerHeight / 607) * 2100;
+      const scale = 2;
       const toCanvas1 = document.querySelector('#toCanvas1') as HTMLElement;
       const toCanvas2 = document.querySelector('#toCanvas2') as HTMLElement;
       const toCanvas3 = document.querySelector('#toCanvas3') as HTMLElement;
-
-      // const toInsert = document.querySelector('#toInsert') as HTMLElement;
-      // elementsArray.forEach((elem) => console.log({ height: elem.clientHeight }));
-
-      // const maxHeight = 1400;
-      // const maxHeight = 2100;
-      const maxHeight = (window.innerHeight / 607) * 2100;
-
-      let count = 0;
       let firstPageComplete = false;
       let secondPageComplete = false;
       let threePageComplete = false;
       let paddingToSecondPage = false;
       let paddingToThirdtPage = false;
+      let count = 0;
 
-      const elements = document.querySelectorAll(
-        '#resume',
-      ) as NodeListOf<HTMLElement>;
+      const cleanElements = this.cleanUpElements(elements);
 
-      elements.forEach((resumeElement) => {
-        const elementsToRemove = resumeElement.querySelectorAll('#remove');
-
-        elementsToRemove.forEach((removeElement) => {
-          removeElement.remove();
-        });
-      });
-
-      const elementsArray = Array.from(elements);
-
-      elementsArray.forEach((elem) => {
-        if (elem.clientHeight > 0) {
-          elem.style.padding = '.5rem 3rem';
-        }
-      });
-
-      elementsArray[0].style.paddingTop = '4rem';
-
-      // elementsArray.forEach((elem) =>
-      //   console.log({ despues: elem.clientHeight }),
-      // );
-
-      elementsArray.forEach((elem, i) => {
+      cleanElements.forEach((elem, i) => {
         const elemnHeight = elem.clientHeight;
 
         count += elemnHeight;
@@ -109,7 +208,7 @@ export class FormComponent {
 
         if (count < maxHeight * 1.5 && firstPageComplete) {
           if (!paddingToSecondPage) {
-            elementsArray[i].style.paddingTop = '4rem';
+            cleanElements[i].style.paddingTop = '4rem';
           }
 
           toCanvas2.appendChild(elem.cloneNode(true));
@@ -118,7 +217,7 @@ export class FormComponent {
 
         if (secondPageComplete) {
           if (!paddingToThirdtPage) {
-            elementsArray[i].style.paddingTop = '4rem';
+            cleanElements[i].style.paddingTop = '4rem';
           }
 
           toCanvas3.appendChild(elem.cloneNode(true));
@@ -126,22 +225,17 @@ export class FormComponent {
         }
       });
 
-      const canvas1 = await html2canvas(toCanvas1, {
-        scale: 2,
-      });
-      const imgHeight1 = (canvas1.height * 210) / canvas1.width;
+      const [canvas1, canvas2, canvas3] = await Promise.all([
+        this.generateCanvas(toCanvas1, scale),
+        this.generateCanvas(toCanvas2, scale),
+        this.generateCanvas(toCanvas3, scale),
+      ]);
 
-      const canvas2 = await html2canvas(toCanvas2, {
-        scale: 2,
-      });
-
-      const imgHeight2 = (canvas2.height * 210) / canvas2.width;
-
-      const canvas3 = await html2canvas(toCanvas3, {
-        scale: 2,
-      });
-
-      const imgHeight3 = (canvas3.height * 210) / canvas3.width;
+      const [imgHeight1, imgHeight2, imgHeight3] = [
+        this.calculateImgHeight(canvas1),
+        this.calculateImgHeight(canvas2),
+        this.calculateImgHeight(canvas3),
+      ];
 
       pdf.addImage(
         canvas1.toDataURL('image/png'),
@@ -181,5 +275,33 @@ export class FormComponent {
     } catch (error) {
       alert('try to summarize your content');
     }
+  }
+
+  cleanUpElements(elements: NodeListOf<HTMLElement>) {
+    elements.forEach((resumeElement) => {
+      const elementsToRemove = resumeElement.querySelectorAll('#remove');
+      elementsToRemove.forEach((removeElement) => removeElement.remove());
+    });
+    const cleanElements = Array.from(elements);
+    cleanElements.forEach((elem) => {
+      if (elem.clientHeight > 0) {
+        elem.style.padding = '.5rem 3rem';
+      }
+    });
+
+    elements[0].style.paddingTop = '4rem';
+
+    return cleanElements;
+  }
+
+  async generateCanvas(
+    container: HTMLElement,
+    scale: number,
+  ): Promise<HTMLCanvasElement> {
+    return html2canvas(container, { scale });
+  }
+
+  calculateImgHeight(canvas: HTMLCanvasElement): number {
+    return canvas ? (canvas.height * 210) / canvas.width : 0;
   }
 }
